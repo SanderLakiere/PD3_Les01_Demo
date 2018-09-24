6235894cd58b95c6dee44a339a55a0b9b60921db
Sander <Sanderdrupal@gmail.com>
2018-09-24 15:48:01 +0200

    Added initial scene setup
    Created a scene with a basic floor and character with character controller component

---
+++ Assets/Scripts/CharacterControllerBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerBehaviour : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
